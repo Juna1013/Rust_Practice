bf5c5e15bb24e4399beaa333f84fee4cd3db068d
Junya <160022550+Juna1013@users.noreply.github.com>
2024-07-28 19:10:57 +0900

    Update sample3-9.rs

--- sample3-9.rs
+++ sample3-9.rs
@@ -1,7 +1,9 @@
-let s = String::from("hello");
-let slice = &s[0..2];
-let slice = &s[0..=2];
-let slice = &s[..2];
-let slice = &s[3..s.len()];
-let slice = &s[3..];
-let slice = &s[..];
+fn main() {
+    let s = String::from("hello");
+    let slice = &s[0..2];
+    let slice = &s[0..=2];
+    let slice = &s[..2];
+    let slice = &s[3..s.len()];
+    let slice = &s[3..];
+    let slice = &s[..];
+}
